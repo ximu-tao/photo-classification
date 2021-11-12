2161653b7a5a2ec8baccb792ecd20b13e7a39cb5
ximu-tao <me_litao@outlook.com>
2021-11-12 12:46:16 +0800

    💄 全局引入 element-ui
    Signed-off-by: ximu-tao <me_litao@outlook.com>

--- app/src/renderer/main.js
+++ app/src/renderer/main.js
@@ -8,6 +8,10 @@ if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+
 /* eslint-disable no-new */
 new Vue({
   components: { App },

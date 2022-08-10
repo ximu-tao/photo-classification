d2c0cacaaeb8eeb8a070377155624a78b93969cf
ximu-tao <me_litao@outlook.com>
2022-08-10 16:46:21 +0800

    ✨ 增加全局设置页面
    Signed-off-by: ximu-tao <me_litao@outlook.com>

---
+++ app/src/renderer/components/SettingContainer.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+
+    <el-button  class="top-right-fixed"
+                type="primary" icon="el-icon-setting"
+                @click="settingVisible = true"></el-button>
+
+    <el-dialog
+        title="设 置"
+        :visible.sync="settingVisible"
+        fullscreen="true"
+        show-close="false"
+    >
+
+      <div class="tip">一些软件的全局设置</div>
+      <el-collapse>
+        <el-collapse-item title="实验性功能" name="1">
+
+        </el-collapse-item>
+
+      </el-collapse>
+
+      <span slot="footer" class="dialog-footer">
+<!--        <el-button @click="settingVisible = false">取 消</el-button>-->
+        <el-button type="primary" @click="settingVisible = false">保存</el-button>
+      </span>
+    </el-dialog>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SettingContainer",
+  data() {
+    return {
+      value: true,
+      settingVisible: false
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+
+
+.tip {
+  padding: 8px 16px;
+  background-color: #ecf8ff;
+  border-radius: 4px;
+  border-left: 5px solid #50bfff;
+  margin: 20px 0;
+
+}
+
+
+.top-right-fixed{
+  position: absolute;
+  top: 0px;
+  right: 0px;
+}
+
+
+
+</style>
